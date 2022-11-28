b67369475abb150794d2f2cbc2c81ae3bef2743e
Ruslan Garapov <ruslangarapov1994@gmail.com>
2022-11-28 22:09:09 +0300

    .services-block-left-item-price

--- src/js/app.js
+++ src/js/app.js
@@ -19,23 +19,25 @@ $(document).ready(function() {
     window.company.init();
     window.faq.init();
 
-    $(window).on('mousemove', function(e) {
-        var x = e.clientX;
-        var y = e.clientY;
-        var windowWidth = $(window).width() / 2;
-        var windowHeight = $(window).height() / 2;
-        $('.translateOnMouseMove').each(function() {
-            var translateSpeed = $(this).attr('data-animateSpeed');
-            $(this).css({
-                transform: `translate( ${ (windowWidth - x)/translateSpeed}px, ${( windowHeight - y )/translateSpeed}px)`
+    if (window.innerWidth > 1320) {
+        $(window).on('mousemove', function(e) {
+            var x = e.clientX;
+            var y = e.clientY;
+            var windowWidth = $(window).width() / 2;
+            var windowHeight = $(window).height() / 2;
+            $('.translateOnMouseMove').each(function() {
+                var translateSpeed = $(this).attr('data-animateSpeed');
+                $(this).css({
+                    transform: `translate( ${ (windowWidth - x)/translateSpeed}px, ${( windowHeight - y )/translateSpeed}px)`
+                });
+            });
+            $('.rotateOnMouseMove').each(function() {
+                var rotateSpeed = $(this).attr('data-animateSpeed');
+                $(this).css({
+                    transform: `rotateX( ${ (windowHeight - y)/rotateSpeed}deg) rotateY(${( windowWidth - x )/rotateSpeed * -1 }deg) translateZ(-300px) scale(1.3)`
+                });
+                console.log(windowWidth, windowHeight)
             });
         });
-        $('.rotateOnMouseMove').each(function() {
-            var rotateSpeed = $(this).attr('data-animateSpeed');
-            $(this).css({
-                transform: `rotateX( ${ (windowHeight - y)/rotateSpeed}deg) rotateY(${( windowWidth - x )/rotateSpeed * -1 }deg) translateZ(-300px) scale(1.3)`
-            });
-            console.log(windowWidth, windowHeight)
-        });
-    });
+    }
 });
